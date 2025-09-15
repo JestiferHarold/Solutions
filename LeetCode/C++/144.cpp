10d55b03f75b7eccbc139bf182a4d586b4843477
JestiferHarold <sanjay.pandian@tutamail.com>
2025-09-15 11:58:12 -0400

    2 questions

---
+++ LeetCode/C++/144.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void traverse(TreeNode* root, vector<int>& a) {
+        if (root == nullptr) return;
+
+        a.push_back(root->val);
+        traverse(root->left, a);
+        traverse(root->right, a);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> v;
+
+        traverse(root, v);
+
+        return v;
+
+    }
+};
---
+++ documentation/docs/what.md
@@ -0,0 +1,7 @@
+# Problems that have been solved in different programming languages
+
+<p> Saving the problems completed here for reference </p>
+
+<p> Feel free to open pull requests <p>
+
+![TinyChef Cute](https://pm1.aminoapps.com/7506/c45b65ab0c9494c61bd9b999abce4552314c0315r1-1300-650v2_hq.jpg)
