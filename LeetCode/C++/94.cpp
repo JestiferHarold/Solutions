a4e6e426f56825137370e5ebb305c0b153933eea
JestiferHarold <sanjay.pandian@tutamail.com>
2025-09-16 11:49:15 -0400

    add 2 questions easy bt

---
+++ LeetCode/C++/94.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<int> v;
+
+    void traversal(TreeNode* node) {
+        if (node == nullptr) return;
+
+        traversal(node->left);
+        v.push_back(node->val);
+        traversal(node->right);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        traversal(root);
+        return this->v;
+
+    }
+};
